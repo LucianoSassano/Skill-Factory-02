3efba0d2ebab7bdf9d2934f9466d110bf8513324
Luciano_Sassano <lucianosassano1@gmail.com>
2020-01-21 10:45:27 -0300

    working with js objects and arrays

--- js/001/script.js
+++ js/001/script.js
@@ -9,20 +9,8 @@ function saludar (nombre,titulo){
 
 saludar(nombre,titulo);*/
 
-/*var nacimineto;
-nacimineto = prompt("ingrese su año de nacimiento");
 
-    function edad(nacimiento){
-        var edad;
-        edad = 2020-nacimineto;
-        console.log("Usted tiene",edad ,"años");
-
-    }*/
-        
-
-
-//modificar para agregar exactitud con meses y dias
-
+/*
 var año = prompt("Ingrese su año de nacimiento");
 var mes = prompt("Ingrese su mes de nacimiento");
 
@@ -30,12 +18,88 @@ var mes = prompt("Ingrese su mes de nacimiento");
         mes--;
         var edad = new Date(año,mes)
         var actual = new Date();
-        var resultYear = actual.getFullYear()- edad.getFullYear();
+        var resultYear = actual.getFullYear() - edad.getFullYear();
         if(mes > actual.getMonth()){
             resultYear--;
         }
         console.log("Usted tiene",resultYear,"años actualmente");
     }
 
-    calcularEdad();
+    calcularEdad();*/
 
+
+    //utilizar operador ternario
+    /*
+    var a = true ? 2 : 0 ;  //si la variable a es true su valor es 2 de lo contrario es 0
+
+    var a =  siSeCumple || siNoSeCumple
+
+    var a = isset(miVariable) ? miVariable : 0; //si mi variable existe ... de lo contrario toma valor 0
+
+    // infinity se utiliza para saber si algo es infinito
+
+
+    var miArreglo = []; //array literal
+    var miArreglo2 = new Array;
+
+    var obj = {};
+    var obj = new Object;
+
+    //declaracion de un obj con sus key values
+
+    var persona = {
+        nombre : "luciano",
+        trabajo :"estudiante",
+        toString : function() {
+            return "nombre:"+this.nombre
+        }
+    }
+
+    //callback!*/
+
+
+    var mes = {
+        nombre,
+        numero,
+        cantDias,
+        toString : function(){
+            return "mes de : " + this.nombre + "numero :" + this.numero + "cantidad de dias:" + this.cantDias
+        }
+
+    }
+
+    var arregloMeses = new Array;
+
+
+    function Calendario (){
+        name = name.toLower();
+
+        var continuar = 'y';
+
+        while(continuar === 'y'){
+            mes = new mes;
+            var name = prompt("ingrese nombre del mes");
+            mes.nombre = name;
+            var number = prompt("ingrese numero del mes");
+            mes.numero = number;
+            var days = prompt("ingrese la cantidad de dias del mes");
+            mes.cantDias = days;
+            arregloMeses.push(mes);
+            var continuar = prompt("para continuar presione y de lo contrario presione n");
+    
+        }
+
+        return arregloMeses;
+
+    }
+
+    function verMeses(){
+
+        var i = 0;
+        while(i > arregloMeses.length()){
+            arregloMeses[i].toString();
+        }
+    }
+
+    Calendario();
+    
