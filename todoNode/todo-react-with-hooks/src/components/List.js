6977b43e29c464dead0e5b4b8f480d2092a482bb
Luciano_Sassano <lucianosassano1@gmail.com>
2020-03-05 23:41:56 -0300

    add task done

---
+++ todoNode/todo-react-with-hooks/src/components/List.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Task from "./Task";
+
+const List = ({ onChangeTaskStatus, onEditTask, tasksList, title }) => (
+  <>
+    <h3>{title}</h3>
+    {tasksList.map((task, key) =>
+      (<Task
+        title = {title}
+        task={task}
+        onChangeState={() => onChangeTaskStatus(task)}
+        onEditTask={() => onEditTask(task)}
+        key={key}
+      />)
+    )}
+  </>
+)
+
+export default List
